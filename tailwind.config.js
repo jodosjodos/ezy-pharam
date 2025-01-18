0acbfc442e36d893c984214773a36f926bee2a22
jodosjodos <jeandedieu2030@gmail.com>
2025-01-18 14:37:19 +0200

    done initializing tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#024220",
+        secondary: "#D6FFD9",
+      }
+    },
+  },
+  plugins: [],
+};
