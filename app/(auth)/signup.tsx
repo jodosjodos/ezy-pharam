d5bbb70fc62b079416ffefddba647eb2c742fb8e
jodosjodos <jeandedieu2030@gmail.com>
2025-01-21 17:11:17 +0200

    [fix] fix layout

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -51,113 +51,119 @@ const SignupPage = () => {
         </View>
       </View>
 
-      <View className="flex flex-col  space-y-[7%] px-[5%]">
-        <View className="flex flex-row items-center ">
-          <Ionicons
-            name="person-outline"
-            size={24}
-            color="rgba(34, 31, 31, 0.4)"
-            style={{ position: "absolute", zIndex: 9, left: "6%" }}
-          />
-          <TextInput
-            className="flex-1 font-poppins_reg border border-stroke-10 bg-[#F9FAFB] p-[3.5%] placeholder:text-start px-[15%] rounded-md text-stroke-40"
-            placeholder="Hospital name"
-            value={hospitalName}
-            onChangeText={setHospitalName}
-          />
-        </View>
-        <View className="flex flex-row items-center">
-          <Pressable
-            className={`flex-1 flex-row  items-center py-[3.5%] font-poppins_reg border border-stroke-10 bg-[#F9FAFB]  rounded-md text-stroke-40 px-[5%] space-x-[7%] ${
-              selected === "email" ? "bg-white" : ""
-            }`}
-            onPress={() => setSelected("email")}
-          >
-            <Fontisto
-              name="email"
-              size={28}
-              color={selected === "email" ? "#407CE2" : "rgba(34, 31, 31, 0.4)"}
-            />
-            <Text
-              className={` ${
-                selected === "email" ? "text-bg_primary" : "text-stroke-40"
-              } font-poppins_reg`}
-            >
-              Email
-            </Text>
-          </Pressable>
-          <Pressable
-            className={`flex-1 flex-row  items-center py-[3.5%] font-poppins_reg border border-stroke-10 bg-[#F9FAFB]  rounded-md text-stroke-40 px-[5%] space-x-[7%] ${
-              selected === "phone" ? "bg-white" : "bg-[#F9FAFB]"
-            }`}
-            onPress={() => setSelected("phone")}
-          >
-            <FontAwesome
-              name="phone"
-              size={28}
-              color={selected === "phone" ? "#407CE2" : "rgba(34, 31, 31, 0.4)"}
-            />
-            <Text
-              className={` ${
-                selected === "phone" ? "text-bg_primary" : "text-stroke-40"
-              } font-poppins_reg`}
-            >
-              Phone
-            </Text>
-          </Pressable>
-        </View>
-        <View className="flex flex-row items-center ">
-          {selected === "phone" ? (
-            <FontAwesome
-              name="phone"
-              size={28}
-              color="rgba(34, 31, 31, 0.4)"
-              style={{ position: "absolute", zIndex: 9, left: "6%" }}
-            />
-          ) : (
-            <Fontisto
-              name="email"
+      <View className="flex flex-col  justify-evenly  h-full px-[5%]">
+        <View className="flex flex-col space-y-[7%]">
+          <View className="flex flex-row items-center ">
+            <Ionicons
+              name="person-outline"
               size={24}
               color="rgba(34, 31, 31, 0.4)"
               style={{ position: "absolute", zIndex: 9, left: "6%" }}
             />
-          )}
-          <TextInput
-            className="flex-1 font-poppins_reg border border-stroke-10 bg-[#F9FAFB] p-[3.5%] placeholder:text-start px-[15%] rounded-md text-stroke-40"
-            placeholder={
-              selected === "email"
-                ? "Enter your email"
-                : "Enter your phone number"
-            }
-            value={email_phone}
-            keyboardType="email-address"
-            onChangeText={setEmail_phone}
-          />
+            <TextInput
+              className="flex-1 font-poppins_reg border border-stroke-10 bg-[#F9FAFB] p-[3.5%] placeholder:text-start px-[15%] rounded-md text-stroke-40"
+              placeholder="Hospital name"
+              value={hospitalName}
+              onChangeText={setHospitalName}
+            />
+          </View>
+          <View className="flex flex-row items-center">
+            <Pressable
+              className={`flex-1 flex-row  items-center py-[3.5%] font-poppins_reg border border-stroke-10 bg-[#F9FAFB]  rounded-md text-stroke-40 px-[5%] space-x-[7%] ${
+                selected === "email" ? "bg-white" : ""
+              }`}
+              onPress={() => setSelected("email")}
+            >
+              <Fontisto
+                name="email"
+                size={28}
+                color={
+                  selected === "email" ? "#407CE2" : "rgba(34, 31, 31, 0.4)"
+                }
+              />
+              <Text
+                className={` ${
+                  selected === "email" ? "text-bg_primary" : "text-stroke-40"
+                } font-poppins_reg`}
+              >
+                Email
+              </Text>
+            </Pressable>
+            <Pressable
+              className={`flex-1 flex-row  items-center py-[3.5%] font-poppins_reg border border-stroke-10 bg-[#F9FAFB]  rounded-md text-stroke-40 px-[5%] space-x-[7%] ${
+                selected === "phone" ? "bg-white" : "bg-[#F9FAFB]"
+              }`}
+              onPress={() => setSelected("phone")}
+            >
+              <FontAwesome
+                name="phone"
+                size={28}
+                color={
+                  selected === "phone" ? "#407CE2" : "rgba(34, 31, 31, 0.4)"
+                }
+              />
+              <Text
+                className={` ${
+                  selected === "phone" ? "text-bg_primary" : "text-stroke-40"
+                } font-poppins_reg`}
+              >
+                Phone
+              </Text>
+            </Pressable>
+          </View>
+          <View className="flex flex-row items-center ">
+            {selected === "phone" ? (
+              <FontAwesome
+                name="phone"
+                size={28}
+                color="rgba(34, 31, 31, 0.4)"
+                style={{ position: "absolute", zIndex: 9, left: "6%" }}
+              />
+            ) : (
+              <Fontisto
+                name="email"
+                size={24}
+                color="rgba(34, 31, 31, 0.4)"
+                style={{ position: "absolute", zIndex: 9, left: "6%" }}
+              />
+            )}
+            <TextInput
+              className="flex-1 font-poppins_reg border border-stroke-10 bg-[#F9FAFB] p-[3.5%] placeholder:text-start px-[15%] rounded-md text-stroke-40"
+              placeholder={
+                selected === "email"
+                  ? "Enter your email"
+                  : "Enter your phone number"
+              }
+              value={email_phone}
+              keyboardType={selected === "email" ? "email-address" : "phone-pad"}
+              onChangeText={setEmail_phone}
+            />
+          </View>
+          <View className="flex flex-row items-center ">
+            <Feather
+              name="lock"
+              size={24}
+              color="rgba(34, 31, 31, 0.4)"
+              style={{ position: "absolute", zIndex: 9, left: "6%" }}
+            />
+            <TextInput
+              className="flex-1 font-poppins_reg border border-stroke-10 bg-[#F9FAFB] p-[3.5%] placeholder:text-start px-[15%] rounded-md text-stroke-40"
+              placeholder="Enter your password"
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry={!showPassword}
+              keyboardType="default"
+            />
+            <FontAwesome
+              name={showPassword ? "eye" : "eye-slash"}
+              size={24}
+              color="rgba(34, 31, 31, 0.4)"
+              style={{ position: "absolute", zIndex: 9, right: "6%" }}
+              onPress={() => setShowPassword(!showPassword)}
+            />
+          </View>
         </View>
-        <View className="flex flex-row items-center ">
-          <Feather
-            name="lock"
-            size={24}
-            color="rgba(34, 31, 31, 0.4)"
-            style={{ position: "absolute", zIndex: 9, left: "6%" }}
-          />
-          <TextInput
-            className="flex-1 font-poppins_reg border border-stroke-10 bg-[#F9FAFB] p-[3.5%] placeholder:text-start px-[15%] rounded-md text-stroke-40"
-            placeholder="Enter your password"
-            value={password}
-            onChangeText={setPassword}
-            secureTextEntry={!showPassword}
-            keyboardType="default"
-          />
-          <FontAwesome
-            name={showPassword ? "eye" : "eye-slash"}
-            size={24}
-            color="rgba(34, 31, 31, 0.4)"
-            style={{ position: "absolute", zIndex: 9, right: "6%" }}
-            onPress={() => setShowPassword(!showPassword)}
-          />
-        </View>
-        <View className="flex flex-row items-center space-x-[2%]">
+        <View className="flex flex-row items-center space-x-[3%] px-[2%]">
           <CheckBox
             className="rounded-md "
             disabled={false}
@@ -189,24 +195,26 @@ const SignupPage = () => {
             </Text>
           </Text>
         </View>
-        <Pressable
-          onPress={() => router.push("/(auth)/signup")}
-          className="bg_primary py-[4%] px-[28%] border border-bg_primary bg-bg_primary rounded-3xl mt-4"
-        >
-          <Text className="text-white  font-poppins_bold text-center">
-            Sign Up
-          </Text>
-        </Pressable>
-        <Text className="text-center font-poppins_reg">
-          Already have an account ?{" "}
-          <Link
-            href="/(auth)/login"
-            className="text-bg_primary font-poppins_reg"
+        <View className="flex flex-col space-y-5">
+          <Pressable
+            onPress={() => router.push("/(auth)/signup")}
+            className="bg_primary py-[4%] px-[28%] border border-bg_primary bg-bg_primary rounded-3xl mt-4"
           >
-            Sign in
-          </Link>
-        </Text>
-        <Pressable className="flex flex-row items-center justify-center space-x-[5%] py-[4%] px-[28%] border border-[#E5E5E5] rounded-md mt-4">
+            <Text className="text-white  font-poppins_bold text-center">
+              Sign Up
+            </Text>
+          </Pressable>
+          <Text className="text-center font-poppins_reg">
+            Already have an account ?{" "}
+            <Link
+              href="/(auth)/login"
+              className="text-bg_primary font-poppins_reg"
+            >
+              Sign in
+            </Link>
+          </Text>
+        </View>
+        <Pressable className="flex flex-row items-center justify-center space-x-[5%] py-[4%] px-[28%] border border-[#E5E5E5] rounded-md mb-4">
           <AntDesign name="google" size={24} color="black" />
           <Text className="text-center font-poppins_bold text-primary">
             Sign In with Google
