232125101b6811fa6b152ffcdb7805833830b0bd
jodosjodos <jeandedieu2030@gmail.com>
2025-01-25 23:17:17 +0200

    done with faq page

--- app/(patient)/(profile)/faq.tsx
+++ app/(patient)/(profile)/faq.tsx
@@ -1,12 +1,60 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { faqs, images } from "@/constants";
+import { handleBack } from "@/utils";
+import { Entypo, FontAwesome } from "@expo/vector-icons";
+import React, { useState } from "react";
+import { View, Text, Pressable, ScrollView, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const FaqPage = () => {
+const FAQ = () => {
+  const [expanded, setExpanded] = useState<number | null>(null);
+
+  const toggleExpand = (index: number) => {
+    setExpanded(expanded === index ? null : index);
+  };
+
   return (
-    <View>
-      <Text>FaqPage</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-white py-4 ">
+      <View className="flex flex-row justify-between px-2 items-center ">
+        <Pressable
+          onPress={handleBack}
+          className="text-[#A1A8B0] self-start  text-xl"
+        >
+          <Image source={images?.Back} className="w-12" />
+        </Pressable>
+        <Text className="text-primary font-poppins_bold text-2xl">FAQ</Text>
+        <FontAwesome name="search" size={24} />
+      </View>
+      <ScrollView className=" py-8 h-full ">
+        <View className=" flex flex-col justify-between py-3">
+          {faqs.map((faq, index) => (
+            <View key={index} className="border-b border-line-13 py-5">
+              <View className="flex flex-row items-center justify-between px-4">
+                <Text className="text-md font-semibold text-primary font-poppins_bold">
+                  {faq.question}
+                </Text>
+                <Pressable onPress={() => toggleExpand(index)}>
+                  <Entypo
+                    name={
+                      expanded === index
+                        ? "chevron-thin-up"
+                        : "chevron-thin-down"
+                    }
+                    size={15}
+                    color="#d1d5db"
+                  />
+                </Pressable>
+              </View>
+              {expanded === index && (
+                <Text className="text-[#757575] font-poppins_reg text-sm m-4">
+                  {faq.answer}
+                </Text>
+              )}
+            </View>
+          ))}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-export default FaqPage
+export default FAQ;
--- constants/data.ts
+++ constants/data.ts
@@ -1,4 +1,4 @@
-import  {Profile} from "./images.js";
+import { Profile } from "./images.js";
 
 export const ProfileData = {
   fullName: "John Doe",
@@ -7,3 +7,45 @@ export const ProfileData = {
   phoneNumber: "0781234567",
   profile: Profile,
 };
+export const faqs = [
+  {
+    question: "What services does the EzyPharma app offer",
+    answer:
+      "Our app serves as a centralized platform for integrated referral hospitals and online pharmacies, offering a range of services. You can conveniently order your medications, receive personalized diet suggestions based on your prescribed medications, set medication reminders, and enjoy medicine delivery at large discount.",
+  },
+  {
+    question: "How do i order medications through the app?",
+    answer:
+      "Our app serves as a centralized platform for integrated referral hospitals and online pharmacies, offering a range of services. You can conveniently order your medications, receive personalized diet suggestions based on your prescribed medications, set medication reminders, and enjoy medicine delivery at large discount.",
+  },
+  {
+    question: "How does the diet suggestion feature work?",
+    answer:
+      "Our app serves as a centralized platform for integrated referral hospitals and online pharmacies, offering a range of services. You can conveniently order your medications, receive personalized diet suggestions based on your prescribed medications, set medication reminders, and enjoy medicine delivery at large discount.",
+  },
+  {
+    question: "How does the reminder system work?",
+    answer:
+      "Our app serves as a centralized platform for integrated referral hospitals and online pharmacies, offering a range of services. You can conveniently order your medications, receive personalized diet suggestions based on your prescribed medications, set medication reminders, and enjoy medicine delivery at large discount.",
+  },
+  {
+    question: "What delivery options are available?",
+    answer:
+      "Our app serves as a centralized platform for integrated referral hospitals and online pharmacies, offering a range of services. You can conveniently order your medications, receive personalized diet suggestions based on your prescribed medications, set medication reminders, and enjoy medicine delivery at large discount.",
+  },
+  {
+    question: "Is my personal information secure?",
+    answer:
+      "Our app serves as a centralized platform for integrated referral hospitals and online pharmacies, offering a range of services. You can conveniently order your medications, receive personalized diet suggestions based on your prescribed medications, set medication reminders, and enjoy medicine delivery at large discount.",
+  },
+  {
+    question: "How can I contact customer support?",
+    answer:
+      "Our app serves as a centralized platform for integrated referral hospitals and online pharmacies, offering a range of services. You can conveniently order your medications, receive personalized diet suggestions based on your prescribed medications, set medication reminders, and enjoy medicine delivery at large discount.",
+  },
+  {
+    question: "How can I reset my password?",
+    answer:
+      "Our app serves as a centralized platform for integrated referral hospitals and online pharmacies, offering a range of services. You can conveniently order your medications, receive personalized diet suggestions based on your prescribed medications, set medication reminders, and enjoy medicine delivery at large discount.",
+  },
+];
