b53b2716b7154a28a40df4baa68a1eb20d9d6b6e
jodosjodos <jeandedieu2030@gmail.com>
2025-02-03 20:12:02 +0200

    [fix] remove unused imports

--- app/(patient)/(hospital)/[id].tsx
+++ app/(patient)/(hospital)/[id].tsx
@@ -1,6 +1,6 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { useLocalSearchParams, useRouter } from "expo-router";
+import { useLocalSearchParams } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Hospital = () => {
